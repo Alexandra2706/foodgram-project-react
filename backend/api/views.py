43b727ca2e93afa209ea2ff685e2101170488050
Александра Гаврилова <cx_76@mail.ru>
2023-04-26 20:21:02 +0300

    flake8 views

--- backend/api/views.py
+++ backend/api/views.py
@@ -6,7 +6,6 @@ from rest_framework import filters, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
-
 from api.filters import RecipeFilter
 from api.pagination import CustomPagination
 from api.permissions import IsAuthorOrReadOnly

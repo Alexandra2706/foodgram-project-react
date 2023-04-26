aa48984b169a4e36eca991f90e7e874a96c52312
Александра Гаврилова <cx_76@mail.ru>
2023-04-26 20:18:57 +0300

    flake8

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,7 +2,6 @@ from django.db.models import Sum
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
-from fpdf import FPDF
 from rest_framework import filters, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny, IsAuthenticated
